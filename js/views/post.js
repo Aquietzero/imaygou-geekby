4deb3ec0d0d91aec696a63c0df1f86d2561b1d7e
Aquietzero <zhaoyunhaosss@gmail.com>
2013-11-11 02:16:26 +0800

    add: rob link.

--- js/views/post.js
+++ js/views/post.js
@@ -8,6 +8,14 @@ define(function (require) {
     className: 'post',
     template: _.template(template),
 
+    events: {
+      'click .rob-button': 'redirectToDetail'
+    },
+
+    redirectToDetail: function () {
+      window.location = '#posts/' + this.model.get('id');
+    },
+
     render: function () {
       this.$el.html(this.template(this.model.attributes));
       this.$el.addClass(this.model.get('post_type').toLowerCase());
